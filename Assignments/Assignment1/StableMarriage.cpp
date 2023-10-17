e4e31b2a1b9c18a8aaf780eaad1065d99aaf35e9
Mingwei Zhang <81440878+mingwzhang@users.noreply.github.com>
2023-10-17 18:00:35 -0400

    update from laptop

---
+++ Assignments/Assignment1/StableMarriage.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+bool ok()
+{
+  int mp[3][3] = {1,2,0,
+                  0,1,2,
+                  1,2,0};
+
+  int wp[3][3] = {2,1,0,
+                  0,2,1,
+                  1,2,0};
+
+
+}
+
+int main()
+{
+  int q[] = {};   // women = value of the index in the array
+
+  int index = 0; // men = index
+
+
+
+
+
+  return 0;
+}
--- Lab/check.cpp
+++ Lab/check.cpp
@@ -1,49 +1,13 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-// Returns true if the number written in square c is ok.
-bool ok(int q[], int c) {
+int main()
+{
+   int x = 42;
+   int* y = &x;
 
-   static int adj[8][5] = {
-        {-1},
-        {0, -1},
-        {0, -1},
-        {0, 1, 2, -1},
-        {0, 1, 3, -1},
-        {1, 4, -1},
-        {2, 3, 4, -1},
-        {3, 4, 5, 6, -1},
-    }; // 7
+   cout << *y << endl;
+   cout << y << endl;
 
-   for (int i = 0; i < c; ++i) // If the number in square c has already been used, return false.
-      if (q[i] == q[c])
-         return false;
-
-   for (int i = 0; adj[c][i] != -1; ++i) // For each neighbor of square c,
-      if (abs(q[c] - q[adj[c][i]]) == 1) // if the numbers written in square c and the neighbor differ by 1, return false.
-         return false;
-   return true;
-}
-
-void print(int q[]) {
-   static int solution = 0;
-   cout << "Solution #" << ++solution << "\n";
-   cout << " "  << q[1] << q[4] << "\n";
-   cout << q[0] << q[2] << q[5] << q[7] << "\n";
-   cout << " "  << q[3] << q[6] << "\n\n";
-}
-
-void next(int q[], int c) {
-   if (c == 8)
-      print(q);
-   else for (q[c] = 1; q[c] <= 8; ++q[c])
-      if (ok(q, c))
-         next(q, c+1);
-}
-
-int main() {
-   int q[8];
-   next(q, 0);
    return 0;
 }
