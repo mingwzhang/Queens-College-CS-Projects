c6137e2faa9f7b495849a0528e9f7b03a00601ea
Mingwei Zhang <mingw.zhang123@gmail.com>
2024-05-09 18:31:08 -0400

    tinyj3 progress made 50% + lisp4 assignment complete

---
+++ CS316/316TinyJ/Tinyj Assignment3/JUMPONFALSEinstr.java
@@ -0,0 +1,16 @@
+package TJasn.virtualMachine;
+
+import static TJasn.virtualMachine.CodeInterpreter.*;
+
+public class JUMPONFALSEinstr extends OneOperandInstruction {
+
+  void execute ()
+  {
+    if(EXPRSTACK[--ESP] == 0) PC = operand;
+  }
+
+  public JUMPONFALSEinstr (int address)
+  {
+    super(address, "JUMPONFALSE");
+  }
+}
---
+++ CS316/316TinyJ/Tinyj Assignment3/JUMPinstr.java
@@ -0,0 +1,16 @@
+package TJasn.virtualMachine;
+
+import static TJasn.virtualMachine.CodeInterpreter.*;
+
+public class JUMPinstr extends OneOperandInstruction {
+
+  void execute ()
+  {
+    PC = operand;
+  }
+
+  public JUMPinstr (int address)
+  {
+    super(address, "JUMP");
+  }
+}
